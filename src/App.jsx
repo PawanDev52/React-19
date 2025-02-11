656db4feb9e4dfa6dcca37dd443e58e2f8625c9a
Pawan sharma <pawansh803@gmail.com>
2025-02-11 22:42:30 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header";
+function Apple() {
+  return (
+    <div>
+      <Header />
+      <h1>Hello</h1>
+      <h1>code with me</h1>
+    </div>
+  );
+}
+
+export default Apple;
