72bc3e79ee8884e015fce141ad1d3e0951e6279b
Pawan sharma <pawansh803@gmail.com>
2025-02-14 22:43:09 +0530

    Events & functions

--- src/App.jsx
+++ src/App.jsx
@@ -34,55 +34,73 @@ import ToDo from "./ToDo";
 // }
 
 // jsx with curly braces
+// function App() {
+//   const name = "React dev";
+//   const userObj = {
+//     name: "developer",
+//     age: 34,
+//     city: "new town",
+//     height: 150,
+//     width: 150,
+//     url: "https://plus.unsplash.com/premium_photo-1722859221349-26353eae4744?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//   };
+//   const userArray = ["sam", "peter", "henry"];
+//   let x = 2;
+//   let y = 7;
+
+//   function fruit() {
+//     return "Apple";
+//   }
+
+//   function sum(a, b) {
+//     return a + b;
+//   }
+
+//   function operation(a, b, op) {
+//     if (op == "+") {
+//       return a + b;
+//     } else if (op == "-") {
+//       return a - b;
+//     } else {
+//       return a * b;
+//     }
+//   }
+//   return (
+//     <div>
+//       <h1>JSX with curly braces</h1>
+//       <h1>{name ? name : "user not found"}</h1>
+//       <h1>{x * y}</h1>
+//       <h1>{fruit()}</h1>
+//       <h1>{sum(5, 45)}</h1>
+//       <h1>{operation(5, 8, "+")}</h1>
+//       <h1>{userObj.city}</h1>
+//       <input type="text" value={name} id={name} />
+//       <h1>{userArray[2]}</h1>
+//       <img
+//         src={userObj.url}
+//         alt="dog"
+//         width={userObj.width}
+//         height={userObj.height}
+//       />
+//     </div>
+//   );
+// }
+
+// click event & functions
 function App() {
-  const name = "React dev";
-  const userObj = {
-    name: "developer",
-    age: 34,
-    city: "new town",
-    height: 150,
-    width: 150,
-    url: "https://plus.unsplash.com/premium_photo-1722859221349-26353eae4744?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  function callFun() {
+    alert("function called inner");
+  }
+  const fruit = (name) => {
+    alert(name);
   };
-  const userArray = ["sam", "peter", "henry"];
-  let x = 2;
-  let y = 7;
-
-  function fruit() {
-    return "Apple";
-  }
-
-  function sum(a, b) {
-    return a + b;
-  }
-
-  function operation(a, b, op) {
-    if (op == "+") {
-      return a + b;
-    } else if (op == "-") {
-      return a - b;
-    } else {
-      return a * b;
-    }
-  }
   return (
     <div>
-      <h1>JSX with curly braces</h1>
-      <h1>{name ? name : "user not found"}</h1>
-      <h1>{x * y}</h1>
-      <h1>{fruit()}</h1>
-      <h1>{sum(5, 45)}</h1>
-      <h1>{operation(5, 8, "+")}</h1>
-      <h1>{userObj.city}</h1>
-      <input type="text" value={name} id={name} />
-      <h1>{userArray[2]}</h1>
-      <img
-        src={userObj.url}
-        alt="dog"
-        width={userObj.width}
-        height={userObj.height}
-      />
+      <h1>Events and Functions</h1>
+      <button onClick={() => callFun()}>Apple</button>
+      <button onClick={() => fruit("orange")}>Orange</button>
     </div>
   );
 }
+
 export default App;
