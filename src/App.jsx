ca799420de8bb36790e3f80a3ea210d07e72f07a
Pawan sharma <pawansh803@gmail.com>
2025-02-17 21:08:14 +0530

    state

--- src/App.jsx
+++ src/App.jsx
@@ -105,9 +105,16 @@ import ToDo from "./ToDo";
 
 // state
 function App() {
+  let fruit = "Apple";
+  const handleFruit = () => {
+    fruit = "Banana";
+    console.log(fruit);
+  };
   return (
     <div>
       <h1>State in React Js</h1>
+      <h1>{fruit}</h1>
+      <button onClick={handleFruit}>Change Fruit Name</button>
     </div>
   );
 }
