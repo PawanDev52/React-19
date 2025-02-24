bb23f37e4546e0a72b0e87b35f1ce2ad0f3b5b5e
Pawan sharma <pawansh803@gmail.com>
2025-02-24 22:21:17 +0530

    Handle radio and dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -317,11 +317,55 @@ import Skills from "./Skills";
 // }
 
 // Handle check boxes
+// function App() {
+//   return (
+//     <div>
+//       <h1>Handle Checkbox in React JS</h1>
+//       <Skills />
+//     </div>
+//   );
+// }
+
+// Handle radio and dropdown
 function App() {
+  const [gender, setGender] = useState("female");
+  const [city, setCity] = useState("delhi");
   return (
     <div>
-      <h1>Handle Checkbox in React JS</h1>
-      <Skills />
+      <h1>Handle Radio and Dropdown</h1>
+      <h4>Select Gender</h4>
+      <input
+        type="radio"
+        onChange={(event) => setGender(event.target.value)}
+        checked={gender == "male"}
+        name="gender"
+        value={"male"}
+        id="male"
+      />
+      <label htmlFor="male">Male</label>
+      <input
+        type="radio"
+        onChange={(event) => setGender(event.target.value)}
+        checked={gender == "female"}
+        name="gender"
+        value={"female"}
+        id="female"
+      />
+      <label htmlFor="female">Female</label>
+      <h2>Selected Gender: {gender}</h2>
+      <br />
+
+      <h4>Select City</h4>
+      <select
+        onChange={(event) => setCity(event.target.value)}
+        defaultValue={"delhi"}
+      >
+        <option value="noida">Noida</option>
+        <option value="gurgaon">Gurgaon</option>
+        <option value="delhi">Delhi</option>
+        <option value="pune">Pune</option>
+      </select>
+      <h2>Selected City: {city}</h2>
     </div>
   );
 }
