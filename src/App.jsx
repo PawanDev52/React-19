65de53c48be02e180c19d5cbb224cdf195fd15cd
Pawan sharma <pawansh803@gmail.com>
2025-03-10 22:57:30 +0530

    component lifecycle

--- src/App.jsx
+++ src/App.jsx
@@ -632,14 +632,19 @@ import CounterUse from "./CounterUse";
 // }
 
 // handle props side effects with useEffect
+// and component life cycle
 function App() {
   const [count, setCount] = useState(0);
   const [data, setData] = useState(0);
+  const [display, setDisplay] = useState(true);
+
   return (
     <div>
-      <CounterUse count={count} data={data} />
+      {display ? <CounterUse count={count} data={data} /> : null}
+
       <button onClick={() => setCount(count + 1)}>Count</button>
       <button onClick={() => setData(data + 1)}>Data</button>
+      <button onClick={() => setDisplay(!display)}>Toggle</button>
     </div>
   );
 }
--- src/CounterUse.jsx
+++ src/CounterUse.jsx
@@ -1,26 +1,51 @@
 import { useEffect } from "react";
 
+// const CounterUse = ({ count, data }) => {
+//   const handleCounter = () => {
+//     console.log("handleCounter called");
+//   };
+
+//   const handleData = () => {
+//     console.log("handleData called");
+//   };
+
+//   useEffect(() => {
+//     handleCounter();
+//   }, []);
+
+//   useEffect(() => {
+//     handleData();
+//   }, [data]);
+
+//   return (
+//     <div>
+//       <h1>Count: {count}</h1>
+//       <h1>Data: {data}</h1>
+//     </div>
+//   );
+// };
+
+// ==========================================================
+// react component lifecycle
 const CounterUse = ({ count, data }) => {
-  const handleCounter = () => {
-    console.log("handleCounter called");
-  };
-
-  const handleData = () => {
-    console.log("handleData called");
-  };
-
   useEffect(() => {
-    handleCounter();
+    console.log("mounting phase only");
   }, []);
 
   useEffect(() => {
-    handleData();
-  }, [data]);
+    console.log("update phase only");
+  }, [count]);
+
+  useEffect(() => {
+    return () => {
+      console.log("unmount phase only");
+    };
+  }, []);
 
   return (
     <div>
-      <h1>Count: {count}</h1>
-      <h1>Data: {data}</h1>
+      <h1>Counter value: {count}</h1>
+      <h1>Data value: {data}</h1>
     </div>
   );
 };
