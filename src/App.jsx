0b11ab3103a07f2b92f3788944646fa829c1b459
Pawan sharma <pawansh803@gmail.com>
2025-03-27 22:45:03 +0530

    Uncontrolled components

--- src/App.jsx
+++ src/App.jsx
@@ -989,39 +989,72 @@ import UserProfile from "./UserProfile";
 // }
 
 // useRef hook
+// function App() {
+//   const inputRef = useRef(null);
+//   const h1Ref = useRef(null);
+
+//   const inputHandler = () => {
+//     console.log(inputRef);
+//     inputRef.current.focus();
+//     inputRef.current.style.color = "red";
+//     inputRef.current.placeholder = "enter password";
+//     inputRef.current.value = "123";
+//   };
+
+//   const toggleHandler = () => {
+//     if (inputRef.current.style.display != "none") {
+//       inputRef.current.style.display = "none";
+//     } else {
+//       inputRef.current.style.display = "inline";
+//     }
+//   };
+
+//   const h1Handler = () => {
+//     h1Ref.current.style.color = "green";
+//   };
+
+//   return (
+//     <>
+//       <h1>useRef</h1>
+//       <button onClick={toggleHandler}>toggle</button>
+//       <input ref={inputRef} type="text" placeholder="Enter user name" />
+//       <button onClick={inputHandler}>Focus on input field</button>
+
+//       <h1 ref={h1Ref}>Web Master</h1>
+//       <button onClick={h1Handler}>Handler</button>
+//     </>
+//   );
+// }
+
+// Uncontrolled Components
 function App() {
-  const inputRef = useRef(null);
-  const h1Ref = useRef(null);
+  const handleForm = (event) => {
+    event.preventDefault();
+    const user = document.querySelector("#user").value;
+    const password = document.querySelector("#password").value;
 
-  const inputHandler = () => {
-    console.log(inputRef);
-    inputRef.current.focus();
-    inputRef.current.style.color = "red";
-    inputRef.current.placeholder = "enter password";
-    inputRef.current.value = "123";
-  };
-
-  const toggleHandler = () => {
-    if (inputRef.current.style.display != "none") {
-      inputRef.current.style.display = "none";
-    } else {
-      inputRef.current.style.display = "inline";
-    }
-  };
-
-  const h1Handler = () => {
-    h1Ref.current.style.color = "green";
+    console.log(user);
+    console.log(password);
   };
 
   return (
     <>
-      <h1>useRef</h1>
-      <button onClick={toggleHandler}>toggle</button>
-      <input ref={inputRef} type="text" placeholder="Enter user name" />
-      <button onClick={inputHandler}>Focus on input field</button>
+      <h1>Uncontrolled Components</h1>
+      <form action="" method="post" onSubmit={handleForm}>
+        <input type="text" id="user" placeholder="Enter user name" />
+        <br />
+        <br />
+        <input
+          type="password"
+          id="password"
+          placeholder="Enter user password"
+        />
+        <br />
+        <br />
+        <button>Submit</button>
+      </form>
 
-      <h1 ref={h1Ref}>Web Master</h1>
-      <button onClick={h1Handler}>Handler</button>
+      <hr />
     </>
   );
 }
