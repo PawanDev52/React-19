febd3b83b013ea1692a71c114e2eb7505b59582b
Pawan sharma <pawansh803@gmail.com>
2025-11-04 22:35:57 +0530

    react js

--- src/App.jsx
+++ src/App.jsx
@@ -19,24 +19,24 @@ function App() {
   const userName = "React Dev";
   let x = 20;
   let y = 30;
-  return (
-    <>
-      <h1>{userName}</h1>
-      <h1>{10 + 20 + 30}</h1>
-      <h1>{x * y}</h1>
-      <button onClick={() => alert("hello")}>Click</button>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <h1>{userName}</h1>
+//       <h1>{10 + 20 + 30}</h1>
+//       <h1>{x * y}</h1>
+//       <button onClick={() => alert("hello")}>Click</button>
+//     </>
+//   );
+// }
 
 // without jsx
-// return createElement("div", {id:"rootDiv"}, "hellow");
+return createElement("div", {id:"rootDiv"}, "hellow");
 
 // with jsx
 // return (
 //   <div className="rootDiv">Hello Div</div>
 // )
-// }
+}
 
 // jsx exercise
 // function App(){
