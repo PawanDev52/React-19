6804bd5ccbf3241af68b15e6e4b8711586b06655
Pawan sharma <pawansh803@gmail.com>
2025-11-07 22:22:37 +0530

    react js

--- src/App.jsx
+++ src/App.jsx
@@ -181,24 +181,24 @@ import { SubjectName } from "./ContextData";
 // }
 
 // else if condition
-function App() {
-  const [count, setCount] = useState(0);
-  return (
-    <div>
-      <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>Counter</button>
-      {
-        count == 0 ? <h1>Condition 0</h1>
-          : count == 1 ? <h1>Condition 1</h1>
-            : count == 2 ? <h1>Condition 2</h1>
-              : count == 3 ? <h1>Condition 3</h1>
-                : count == 4 ? <h1>Condition 4</h1>
-                  : count == 5 ? <h1>Condition 5</h1>
-                    : <h1>Other Condition</h1>
-      }
-    </div>
-  )
-}
+// function App() {
+//   const [count, setCount] = useState(0);
+//   return (
+//     <div>
+//       <h1>{count}</h1>
+//       <button onClick={() => setCount(count + 1)}>Counter</button>
+//       {
+//         count == 0 ? <h1>Condition 0</h1>
+//           : count == 1 ? <h1>Condition 1</h1>
+//             : count == 2 ? <h1>Condition 2</h1>
+//               : count == 3 ? <h1>Condition 3</h1>
+//                 : count == 4 ? <h1>Condition 4</h1>
+//                   : count == 5 ? <h1>Condition 5</h1>
+//                     : <h1>Other Condition</h1>
+//       }
+//     </div>
+//   )
+// }
 
 // props
 // function App() {
@@ -342,14 +342,14 @@ function App() {
 // }
 
 // Handle check boxes
-// function App() {
-//   return (
-//     <div>
-//       <h1>Handle Checkbox in React JS</h1>
-//       <Skills />
-//     </div>
-//   );
-// }
+function App() {
+  return (
+    <div>
+      <h1>Handle Checkbox in React JS</h1>
+      <Skills />
+    </div>
+  );
+}
 
 // Handle radio and dropdown
 // function App() {
